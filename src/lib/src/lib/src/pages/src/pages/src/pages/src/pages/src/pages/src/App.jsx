7507be520ff40f740d0477183cc72c9c1f1dfd94
samuelryan-art <wenonehsamuelryan@gmail.com>
2025-12-16 13:57:25 +0100

    Add App.jsx

---
+++ src/lib/src/lib/src/pages/src/pages/src/pages/src/pages/src/pages/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Cards from "./pages/Cards";
+import Marketplace from "./pages/Marketplace";
+import Settings from "./pages/Settings";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/marketplace" element={<Marketplace />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;

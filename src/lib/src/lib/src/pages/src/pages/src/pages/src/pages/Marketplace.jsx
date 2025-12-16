f7d40a57ea5adb650d0d1e8f5e08d881c7b620b7
samuelryan-art <wenonehsamuelryan@gmail.com>
2025-12-16 13:54:28 +0100

    Add Marketplace.jsx

---
+++ src/lib/src/lib/src/pages/src/pages/src/pages/src/pages/Marketplace.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Marketplace = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Marketplace</h2>
+      <p>Explore offers, promotions, and partner services here.</p>
+      {/* You can later add product listings or payment options */}
+    </div>
+  );
+};
+
+export default Marketplace;

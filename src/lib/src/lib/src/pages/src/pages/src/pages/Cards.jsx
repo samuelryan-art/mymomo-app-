c3f8a88c057e3d33be18b4e41aaf7f230d3c0f36
samuelryan-art <wenonehsamuelryan@gmail.com>
2025-12-16 13:52:51 +0100

    Add Cards.jsx

---
+++ src/lib/src/lib/src/pages/src/pages/src/pages/Cards.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Virtual Cards</h2>
+      <p>Here you can see your virtual cards, add new ones, or delete existing cards.</p>
+      {/* Add card list, delete buttons, or PIN verification later */}
+    </div>
+  );
+};
+
+export default Cards;

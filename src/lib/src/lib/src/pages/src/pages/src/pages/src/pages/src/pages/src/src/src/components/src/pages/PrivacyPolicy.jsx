ab190c4c51300aaa267be73baab3b791d0d28342
samuelryan-art <wenonehsamuelryan@gmail.com>
2025-12-16 14:21:00 +0100

    Add PrivacyPolicy.jsx

---
+++ src/lib/src/lib/src/pages/src/pages/src/pages/src/pages/src/pages/src/src/src/components/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Privacy Policy</h2>
+      <p>
+        Your privacy is important to us. We do not share your personal data with third parties
+        without your consent.
+      </p>
+      <p>
+        All payments and card information are stored securely.
+      </p>
+      <p>
+        By using MyMoMo, you agree to our terms and privacy practices.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
